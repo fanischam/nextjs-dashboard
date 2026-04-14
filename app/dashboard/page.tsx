9bd0c812890e13937b35219e9b64b810c316eb83
Fanis Chamalidis <fchamalidis@yahoo.com>
2026-04-14 15:53:25 +0300

    added latest invoices in the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import { fetchRevenue } from "@/app/lib/data";
+import { fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
 
     return (
         <main>
@@ -15,6 +17,7 @@ export default async function Page() {
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     )
